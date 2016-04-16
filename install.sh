5e5b931c5cf28d56f9904a15e9d2d05047a97041
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2016-04-16 16:15:33 +0200

    feat(bash): install `nvm`

--- install.sh
+++ install.sh
@@ -4,5 +4,11 @@ for file in .{bash_logout,bash_profile,bash_profile}; do
   ln -si $HOME/dotfiles/$file $HOME/$file
 done
 
-git clone git@github.com:rupa/z.git $HOME/dotfiles/z &&
-  cd $_ && git checkout `git describe --abbrev=0 --tags`
+checkout git@github.com:rupa/z.git z
+
+checkout git@github.com:creationix/nvm.git nvm
+
+function checkout {
+  git clone $1 $HOME/dotfiles/$2 &&
+    cd $_ && git checkout `git describe --abbrev=0 --tags`
+}
