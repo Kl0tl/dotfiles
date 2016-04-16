788555c4b8c8b05bea5038a2780d0ef97f4c670b
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2016-04-17 00:10:46 +0200

    feat(bash): install `z`

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 for file in .{bash_logout,bash_profile,bash_profile}; do
   ln -si $HOME/dotfiles/$file $HOME/$file
 done
+
+git clone git@github.com:rupa/z.git $HOME/dotfiles/z &&
+  cd $_ && git checkout `git describe --abbrev=0 --tags`
